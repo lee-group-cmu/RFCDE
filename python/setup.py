bb027881de88414f98c9929541aac2e7d04a6518
Taylor Pospisil <tpospisi@andrew.cmu.edu>
2018-03-18 14:42:09 -0400

    Bump pypi version

--- python/setup.py
+++ python/setup.py
@@ -28,7 +28,7 @@ for e in ext_modules:
     e.cython_directives = {"embedsignature": True}
 
 setup(name="rfcde",
-      version="0.1",
+      version="0.1.1",
       license="MIT",
       description="Fits random forest conditional density estimate using conditional density loss for splits.",
       long_description = README_TEXT,
