ab9458d417f27706c3f6bddce49fb93908c69071
Taylor Pospisil <tpospisi@andrew.cmu.edu>
2019-05-01 13:52:25 -0400

    Kill warning about np.matrix

--- python/tests/test_basis.py
+++ python/tests/test_basis.py
@@ -2,8 +2,9 @@ import numpy as np
 import rfcde
 import pytest
 
+
 def test_cosine_basis_is_orthonormal():
-    n_grid  = 10000
+    n_grid = 10000
     n_basis = 31
 
     grid = np.linspace(0, 1, n_grid)
@@ -15,15 +16,16 @@ def test_cosine_basis_is_orthonormal():
     np.fill_diagonal(norms, 0.0)
     assert np.abs(norms).max() == pytest.approx(0.0, abs=1e-3)
 
+
 def test_tensor_basis_is_orthonormal():
-    n_grid  = [100, 100]
+    n_grid = [100, 100]
     n_basis = [15, 15]
     basis_fn = rfcde.basis_functions.cosine_basis
 
-    gx, gz = np.meshgrid(np.linspace(0, 1, n_grid[0], endpoint = False),
-                         np.linspace(0, 1, n_grid[1], endpoint = False))
+    gx, gz = np.meshgrid(np.linspace(0, 1, n_grid[0], endpoint=False),
+                         np.linspace(0, 1, n_grid[1], endpoint=False))
 
-    grid = np.matrix([gx.flatten(), gz.flatten()]).T
+    grid = np.array([gx.flatten(), gz.flatten()]).T
 
     basis = rfcde.basis_functions.tensor_basis(grid, n_basis, basis_fn)
     norms = np.matmul(basis.T, basis) / np.prod(n_grid)
